552316680df67f72212dbff4b1590026ab2872fd
spatankar38 <spatankar38@gmail.com>
2018-10-09 21:25:51 -0700

    Test

--- src/main/java/io/openshift/booster/service/Greeting.java
+++ src/main/java/io/openshift/booster/service/Greeting.java
@@ -18,7 +18,7 @@ package io.openshift.booster.service;
 // tag::snippet-greeting[]
 public class Greeting {
 
-    public static final String FORMAT = "Hello, %s!";
+    public static final String FORMAT = "Hello, this is , %s! 's api";
 
     private final String content;
 
